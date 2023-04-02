ec3877c6b878468e019d5ccfc6c655d6d3d79902
dumanmonster <dybysbayd@gmail.com>
2023-04-02 17:45:16 +0600

    dd

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,10 +1,7 @@
 import axios from 'axios'
 const apiClient = axios.create({
-  baseURL: 'http://localhost:3000/api'
+  baseURL: 'https://birdsbuiildapi.herokuapp.com/'
 })
 
-apiClient.interceptors.request.use((config) => {
-  config.headers['Access-Control-Allow-Origin'] = '*'
-  return config
-})
+
 export default apiClient;
